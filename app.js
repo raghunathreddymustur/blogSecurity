f353abecaf8b8d347c52f3133d37bcf882364df0
musturraghunathreddy <raghunathreddymustur321@gmail.com>
2020-06-02 13:02:55 +0530

    add hashing and salting

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyparser=require("body-parser");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const app=express();
 
 app.use(express.static("public"));
@@ -36,24 +37,27 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+  bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(!err){
+        console.log("success");
+        res.render("secrets")
+      }
+      else{
+        console.log(err);
+      }
+    });
   });
-  newUser.save(function(err){
-    if(!err){
-      console.log("success");
-      res.render("secrets")
-    }
-    else{
-      console.log(err);
-    }
-  });
+
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:username},function(err,foundUser){
 
     if(err)
@@ -64,11 +68,13 @@ app.post("/login",function(req,res){
     else{
       if(foundUser)
       {
-        if(foundUser.password===password)
-        {
-          console.log(foundUser);
-          res.render("secrets");
-        }
+        bcrypt.compare(password,foundUser.password,function(err,results){
+          if(results===true)
+          {
+            res.render("secrets");
+          }
+        });
+        
       }
     }
   });
