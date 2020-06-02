162edd06282179e8a9a4b837885241569ec81936
musturraghunathreddy <raghunathreddymustur321@gmail.com>
2020-06-02 12:11:44 +0530

    add hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyparser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 const app=express();
 
 app.use(express.static("public"));
@@ -19,9 +19,7 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-console.log(process.env.SECRET);
-var secret=process.env.SECRET
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+
 const User=new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
@@ -40,7 +38,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -55,7 +53,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:username},function(err,foundUser){
 
     if(err)
